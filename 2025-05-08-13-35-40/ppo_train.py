092b8ecb3d90b8c0d848deaa4767ca558c4ae9f1
Botezatu Cosmin-Adrian <cosmin-adrian.botezatu@s.unibuc.ro>
2025-06-28 12:57:29 +0300

    Modificari la nivelul orasului mic

--- 2025-05-08-13-35-40/ppo_train.py
+++ 2025-05-08-13-35-40/ppo_train.py
@@ -3,6 +3,7 @@ import time
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.vec_env import VecNormalize
 from sumo_env import SumoEnv
 
 # === Config ===
